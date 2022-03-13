e1ebb0d9a87fc8dc8d01bdb69b0e6ea259f99876
ET-TOUNANI <tounaniabderrahmane99@gmail.com>
2022-03-13 01:27:00 +0100

    fix problem with class linked list

--- main.cpp
+++ main.cpp
@@ -22,10 +22,95 @@
 
  */
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 using namespace std;
 
+// type Node
+
+typedef int element_t;
+
+typedef struct noed
+{
+    element_t data;
+    struct noed *next;
+} noed;
+typedef struct liste_t
+{
+    noed *head;
+    noed *last;
+    int length;
+
+} liste_t;
+
+// class Linked_
+class Linked_List
+{
+private:
+    liste_t l;
+
+public:
+    Linked_List()
+    {
+        l.length = 0;
+        l.head = NULL;
+        l.last = NULL;
+    }
+    Linked_List(element_t e)
+    {
+        l.length = 0;
+        l.last = (noed *)malloc(sizeof(noed));
+        l.head = (noed *)malloc(sizeof(noed));
+        l.head->data = e;
+        l.last->data = e;
+        l.head->next = NULL;
+        l.last->next = NULL;
+        l.length++;
+    }
+    void insert_fin(element_t e)
+    {
+        if (l.length > 0)
+        {
+            noed *curr = l.head;
+            while (curr->next != NULL)
+            {
+                curr = curr->next;
+            }
+            curr->next = (noed *)malloc(sizeof(noed));
+            l.last = curr->next;
+            l.last->data = e;
+            l.last->next = NULL;
+            l.length++;
+            return;
+        }
+        l.head = (noed *)malloc(sizeof(noed));
+        l.last = (noed *)malloc(sizeof(noed));
+        l.head->data = e;
+        l.last->data = e;
+        l.head->next = NULL;
+        l.last->next = NULL;
+    }
+    void read()
+    {
+        noed *curr = l.head;
+        while (curr != NULL)
+        {
+            cout << curr->data << " -> ";
+            curr = curr->next;
+        }
+        cout << endl;
+    }
+    /*void delete_()
+    {
+    }*/
+};
 int main()
 {
-
+    Linked_List *v = new Linked_List(55);
+    v->insert_fin(41);
+    v->insert_fin(47);
+    v->insert_fin(44);
+    v->insert_fin(40);
+    v->read();
     return 0;
 }
